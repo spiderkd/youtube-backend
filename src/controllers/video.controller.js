b4b736a9e3d043665cf302bd612abb9d1a69f687
Yash <yashkd12790@gmail.com>
2024-05-25 19:06:53 +0530

    updated videos controller

--- src/controllers/video.controller.js
+++ src/controllers/video.controller.js
@@ -9,11 +9,9 @@ import {
   uploadOnCloudinary,
 } from "../utils/cloudinary.js";
 
-
 const getAllVideos = asyncHandler(async (req, res) => {
   const { page = 1, limit = 10, query, sortBy, sortType, userId } = req.query;
   //TODO: get all videos based on query, sort, pagination
-  
 });
 
 const publishAVideo = asyncHandler(async (req, res) => {
@@ -63,15 +61,128 @@ const getVideoById = asyncHandler(async (req, res) => {
     throw new ApiError(400, "video field empty");
   }
 
-  const video = await Video.findById(videoId);
+  const videoCheck = await Video.findById(videoId);
 
-  if (!video) {
+  if (!videoCheck) {
     throw new ApiError(400, "video id not found");
   }
 
+  const video = await Video.aggregate([
+    {
+      $match: {
+        _id: new mongoose.Types.ObjectId(videoId),
+      },
+    },
+    {
+      $lookup: {
+        from: "likes",
+        localField: "_id",
+        foreignField: "video",
+        as: "likes",
+      },
+    },
+    {
+      $lookup: {
+        from: "users",
+        localField: "owner",
+        foreignField: "_id",
+        as: "owner",
+        pipeline: [
+          {
+            $lookup: {
+              from: "subscriptions",
+              localField: "_id",
+              foreignField: "channel",
+              as: "subscribers",
+            },
+          },
+          {
+            $addFields: {
+              subscribersCount: {
+                $size: "$subscribers",
+              },
+              isSubscribed: {
+                $cond: {
+                  if: {
+                    $in: [req.user?._id, "$subscribers.subscriber"],
+                  },
+                  then: true,
+                  else: false,
+                },
+              },
+            },
+          },
+          {
+            $project: {
+              username: 1,
+              avatar: 1,
+              subscribersCount: 1,
+              isSubscribed: 1,
+            },
+          },
+        ],
+      },
+    },
+    {
+      $addFields: {
+        likesCount: {
+          $size: "$likes",
+        },
+        owner: {
+          $first: "$owner",
+        },
+        isLiked: {
+          $cond: {
+            if: { $in: [req.user?._id, "$likes.likedBy"] },
+            then: true,
+            else: false,
+          },
+        },
+      },
+    },
+    {
+      $project: {
+        videoFile: 1,
+        title: 1,
+        description: 1,
+        views: 1,
+        createdAt: 1,
+        duration: 1,
+        comments: 1,
+        owner: 1,
+        likesCount: 1,
+        isLiked: 1,
+      },
+    },
+  ]);
+
+  if (!video) {
+    throw new ApiError(500, "failed to fetch video");
+  }
+
+  // increment views if video fetched successfully
+  await Video.findByIdAndUpdate(videoId, {
+    $inc: {
+      views: 1,
+    },
+  });
+
+  // add this video to user watch history
+  await User.findByIdAndUpdate(req.user?._id, {
+    $addToSet: {
+      watchHistory: videoId,
+    },
+  });
+
   return res
     .status(200)
-    .json(new ApiResponse(200, { video }, "video fetched success"));
+    .json(
+      new ApiResponse(
+        200,
+        { video },
+        "video details fetched properly fetched success"
+      )
+    );
   //TODO: get video by id
 });
 
@@ -92,6 +203,9 @@ const updateVideo = asyncHandler(async (req, res) => {
   if (!video) {
     throw new ApiError(400, "video missing");
   }
+
+  const delVideo = await Video.findById(videoId);
+  await deleteFromCloudinary(delVideo.thumbnail);
 
   const thumbnailLocalPath = req.file?.path;
 
